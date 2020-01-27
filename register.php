fd2db2b2db9761b69fc6df380329210194953d39
Tim Squires <antiockus@gmail.com>
2020-01-26 22:25:53 -0500

    got register working

--- register.php
+++ register.php
@@ -1,5 +1,32 @@
 <?php
-    require "bootstrap.php";
+
+    use Doctrine\DBAL\DriverManager;
+
+    require 'bootstrap.php';
+
+
+
+    $nickname = htmlspecialchars($_POST['nickname']);
+    $email = htmlspecialchars($_POST['email']);
+    $password = $_POST['password'];
+    $hashed_password = password_hash($password, PASSWORD_DEFAULT);
+
+    $sql = "INSERT INTO USERS ( nickname, email, password)  VALUES (?,?,?)";
+    $connectionParams = [
+        'dbname' => $_ENV['DB_NAME'],
+        'user' => $_ENV['DB_USER'],
+        'password' => $_ENV['DB_PASS'],
+        'host' => $_ENV['DB_HOST'],
+        'port' => $_ENV['DB_PORT'],
+        'driver' => 'pdo_mysql'
+    ];
+
+    $conn = DriverManager::getConnection($connectionParams);
+    $stmt = $conn->prepare($sql);
+    $stmt->bindParam(1, $_POST['nickname']);
+    $stmt->bindParam(2, $_POST['email']);
+    $stmt->bindParam(3, $hashed_password);
+    $stmt->execute();
 
 
     header('Location:/dashboard');
