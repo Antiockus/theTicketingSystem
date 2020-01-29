9d039c888c552510ebd444f8a6bdfe1b3542227c
Tim Squires <antiockus@gmail.com>
2020-01-29 06:43:52 -0500

    adding redirect if fields empty on registration form

--- register.php
+++ register.php
@@ -1,32 +1,40 @@
 <?php
 
-    use Doctrine\DBAL\DriverManager;
+use Doctrine\DBAL\DriverManager;
+use Antiockus\Router;
 
-    require 'bootstrap.php';
+require 'bootstrap.php';
+
+$errors = [];
+$sql = "INSERT INTO USERS ( nickname, email, password)  VALUES (?,?,?)";
+$connectionParams = [
+    'dbname' => $_ENV['DB_NAME'],
+    'user' => $_ENV['DB_USER'],
+    'password' => $_ENV['DB_PASS'],
+    'host' => $_ENV['DB_HOST'],
+    'port' => $_ENV['DB_PORT'],
+    'driver' => 'pdo_mysql'
+];
+$conn = DriverManager::getConnection($connectionParams);
+
+$nickname = htmlspecialchars($_POST['nickname']);
+$email = htmlspecialchars($_POST['email']);
+$password = $_POST['password'];
+$hashed_password = password_hash($password, PASSWORD_DEFAULT);
+
+if (empty($nickname) || empty($email) || empty($password)) {
+    header('Location:/register');
+    exit;
+}
 
 
 
-    $nickname = htmlspecialchars($_POST['nickname']);
-    $email = htmlspecialchars($_POST['email']);
-    $password = $_POST['password'];
-    $hashed_password = password_hash($password, PASSWORD_DEFAULT);
 
-    $sql = "INSERT INTO USERS ( nickname, email, password)  VALUES (?,?,?)";
-    $connectionParams = [
-        'dbname' => $_ENV['DB_NAME'],
-        'user' => $_ENV['DB_USER'],
-        'password' => $_ENV['DB_PASS'],
-        'host' => $_ENV['DB_HOST'],
-        'port' => $_ENV['DB_PORT'],
-        'driver' => 'pdo_mysql'
-    ];
-
-    $conn = DriverManager::getConnection($connectionParams);
-    $stmt = $conn->prepare($sql);
-    $stmt->bindParam(1, $_POST['nickname']);
-    $stmt->bindParam(2, $_POST['email']);
-    $stmt->bindParam(3, $hashed_password);
-    $stmt->execute();
+$stmt = $conn->prepare($sql);
+$stmt->bindParam(1, $_POST['nickname']);
+$stmt->bindParam(2, $_POST['email']);
+$stmt->bindParam(3, $hashed_password);
+$stmt->execute();
 
 
-    header('Location:/dashboard');
+header('Location:/dashboard');
